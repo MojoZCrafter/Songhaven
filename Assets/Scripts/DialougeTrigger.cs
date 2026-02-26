7c60c4d8cd66c58c2574216d1b0ba20b58e2948c
Dogwizard299 <wattssam002@alpinesd.org>
2026-02-26 10:22:32 -0700

    Made the Old Man's dialogue appear when you enter near the screen transition

---
+++ Assets/Scripts/DialougeTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject dialogueBoxUI;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (dialogueBoxUI != null)
+        {
+            dialogueBoxUI.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueBoxUI != null)
+            {
+                dialogueBoxUI.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueBoxUI != null)
+            {
+                dialogueBoxUI.SetActive(false);
+            }
+        }
+    }
+}
