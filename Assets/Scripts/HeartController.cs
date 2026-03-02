abedceaeaa574d878d7d85de6111bb5b16beceb8
MojoZCrafter <adbackman37@gmail.com>
2026-03-02 10:17:18 -0700

    start of heart things

---
+++ Assets/Scripts/HeartController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartController : MonoBehaviour
+{
+
+    PlayerController player;
+    private GameObject[] heartContainers;
+    private Image[] heartFills;
+    public Transform heartsParent;
+    public GameObject heartContainerPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
